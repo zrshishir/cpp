ce533cd10a85b2a67a9498d3d1498907aa6953cc
xs-ziaur <ziaur@xpeedstudio.com>
2022-09-19 20:24:05 +0600

    environment setup completed

---
+++ hacckerrank/language/2_input_var.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    string firstName, lastName;
+
+    cout<<"Please input your first name: ";
+    cin>>firstName;
+    cout<<"Please input your last name: ";
+    cin>>lastName;
+
+    cout<<"you full name is: " <<firstName<<" "<<lastName<<endl;
+
+    return 0;
+}
